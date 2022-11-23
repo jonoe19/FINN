3d79abb4dc44d648f5169198eee6e892d02a30da
lucas02061 <lucasdyssel@gmail.com>
2022-11-23 09:56:31 +0100

    working datagen

--- datagen.py
+++ datagen.py
@@ -4,110 +4,152 @@ import skimage.exposure
 import numpy as np
 import sys
 from numpy.random import default_rng
+from tqdm import tqdm
 
 class DataGen():
+    '''
+    Init
+    '''
     def __init__(self, image_dir, output_dir, dimension = (256, 256)) -> None:
         self.image_dir = image_dir
         self.output_dir = output_dir
         self.dim = dimension
 
-
+    '''
+    Given an RGB image, returns a binary representation of the image.
+    Pixels are split at a luminocity of 200.
+    '''
     def rgb2binary(self, image):
         # grayscale to binary
         thresh = 200
         bw_img = cv2.threshold(image, thresh, 255, cv2.THRESH_BINARY)[1]
         return bw_img
     
+
+    '''
+    Scales the given image to the size provided in the dimension attribute
+    '''
     def scaleimg(self, image):
         return cv2.resize(image, self.dim, interpolation= cv2.INTER_LINEAR)
 
+    '''
+    Generates a random 'hole' mask and overlays it on the provided image.
+    '''
     def addHole(self, image):
-        # generate noise
-        rng = default_rng(seed=np.random.randint(0, sys.maxsize))
-        noise = rng.integers(0, 255, self.dim, np.uint8, True)
+        # For
+        pixels = False
+        while(not pixels):
+            # generate noise
+            rng = default_rng(seed=np.random.randint(0, sys.maxsize))
+            noise = rng.integers(0, 255, self.dim, np.uint8, True)
 
-        # blur the noise to control size
-        blur = cv2.GaussianBlur(noise, (0,0), sigmaX=30, sigmaY=30, borderType = cv2.BORDER_DEFAULT)
+            # blur the noise to control size
+            blur = cv2.GaussianBlur(noise, (0,0), sigmaX=30, sigmaY=30, borderType = cv2.BORDER_DEFAULT)
 
-        # stretch the blurred image to full dynamic range
-        stretch = skimage.exposure.rescale_intensity(blur, in_range='image', out_range=(0,200)).astype(np.uint8)
+            # stretch the blurred image to full dynamic range
+            stretch = skimage.exposure.rescale_intensity(blur, in_range='image', out_range=(0,200)).astype(np.uint8)
 
-        # threshold stretched image to control the size
-        thresh = cv2.threshold(stretch, 130, 255, cv2.THRESH_BINARY)[1]
+            # threshold stretched image to control the size
+            thresh = cv2.threshold(stretch, 130, 255, cv2.THRESH_BINARY)[1]
 
-        # apply morphology open and close to smooth out and make 3 channels
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9,9))
-        mask = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-        mask = cv2.merge([mask,mask,mask])
-        mask = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
+            # apply morphology open and close to smooth out and make 3 channels
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9,9))
+            mask = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+            mask = cv2.merge([mask,mask,mask])
+            mask = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
 
+            pixels = self.check4Hole(mask)
         # add mask to input
         result = cv2.add(image, mask)
 
         return result
-        
-    def augmentFence(self, image):
-        dim = image.shape
-        # flipping
-        i = np.random.randint(-1, 2)
-        if i != 2:
-            image = cv2.flip(image, i) 
-
-        # rotating
-        j = np.random.randint(0,3)
-        for i in range(0,j): 
-            image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-        
-        # warping
-        pix = 5
-        print(dim)
-        pt_A = [np.random.randint(0,pix),np.random.randint(0,pix)]
-        pt_B = [dim[0]-np.random.randint(0,pix),np.random.randint(0,pix)]
-        pt_C = [np.random.randint(0,pix),dim[1]-np.random.randint(0,pix)]
-        pt_D = [dim[0]-np.random.randint(0,pix),dim[1]-np.random.randint(0,pix)]
-        inputpoints = np.float32([pt_A, pt_B, pt_C, pt_D])
-        print('in',inputpoints)
-        outputpoints = np.float32([[0,0],[dim[0],0],[0,dim[1]], [dim[0],dim[1]]])
-        print('out', outputpoints)
-
-        M = cv2.getPerspectiveTransform(inputpoints,outputpoints)
-        image = cv2.warpPerspective(image,M,(dim[1], dim[0]),flags=cv2.INTER_LINEAR)
-        return image
     
-    def test(self, num_images):
-        i = 0
-        while (i <= num_images):
+    def check4Hole(self, mask) -> bool:
+        mask_pixels = mask.sum()/255
+        if mask_pixels > 500 and mask_pixels < 30000:
+            return True
+        return False
+
+    '''
+    Generates new fence patterns by applying mulitple rotations
+    '''
+
+    def augmentFence(self, image):
+        augmented_images = []
+        for i in range(-1,3):
+            augmented_image = image
+            if i != 2:
+                augmented_image = cv2.flip(image, i)
+            for j in range(0,4):
+                final_image = augmented_image
+                for i in range(0,j): 
+                    final_image = cv2.rotate(final_image, cv2.ROTATE_90_CLOCKWISE)
+                augmented_images.append(final_image)
+        return augmented_images
+
+
+    '''
+    Returns a list containing the background image and a mirrored version of it
+    '''
+    def augmentBG(self, image):
+        return [image, cv2.flip(image, 1)]
+        
+    '''
+    Iterates over rural images applying data augmentation to obtain an adequate dataset for the training process.
+    '''
+    def image_gen(self) -> None:
+        total_tqdm = 35*2*8*16*2
+        with tqdm(total=total_tqdm) as pbar:
+            total_rural_index = 0
             for rural in glob.glob(self.image_dir+"/rural"+"/*"):
                 rural_gray = cv2.imread(rural, cv2.IMREAD_GRAYSCALE)
                 rural_scaled_img = self.scaleimg(rural_gray)
+                rural_augmentation_list = self.augmentBG(rural_scaled_img)
+                total_fence_index = 0
                 for fence in glob.glob(self.image_dir+"/fence"+"/*"):
                     # fence
                     fence_gray = cv2.imread(fence, cv2.IMREAD_GRAYSCALE)
                     fence_binary_img = self.rgb2binary(fence_gray)
-                    fence_augmented_img = self.augmentFence(fence_binary_img)
-                    fence_scaled_img = self.scaleimg(fence_augmented_img)
-                    fence_holed_img = self.addHole(fence_scaled_img)
+                    fence_scaled_img = self.scaleimg(fence_binary_img)
+                    fence_augmented_img_list = self.augmentFence(fence_scaled_img)
+                    fence_augmentation_list_hole = []
+                    fence_augmentation_list_nohole = []
+                    for img in fence_augmented_img_list:
+                        fence_augmentation_list_hole.append(self.addHole(img))
+                        fence_augmentation_list_nohole.append(img)
 
                     # rural
-                    rows, cols = rural_scaled_img.shape
-                    rural_scaled_img = rural_scaled_img[0:rows, 0:cols]
-                    mask = cv2.bitwise_or(rural_scaled_img, rural_scaled_img, mask=fence_holed_img)
-                    result = cv2.add(rural_scaled_img, mask)
-                    result = cv2.addWeighted(rural_scaled_img, 0.4, mask, 1.0, 0)
+                    rural_index = 0 # For naming the output file
+                    for rural_img in rural_augmentation_list:
+                        rows, cols = rural_img.shape
+                        rural_img = rural_img[0:rows, 0:cols]
+                        
+                        fence_index = 0
+                        for fence_img in fence_augmentation_list_hole:
+                            mask = cv2.bitwise_or(rural_scaled_img, rural_scaled_img, mask=fence_img)
+                            result = cv2.add(rural_img, mask)
+                            result = cv2.addWeighted(rural_img, 0.4, mask, 1.0, 0)
+                            cv2.imwrite(f'{self.output_dir}/R{total_rural_index}r{rural_index}F{total_fence_index}f{fence_index}withhole.png', result)
+                            fence_index += 1
+                            pbar.update(1)
+
+                        fence_index = 0
+                        for fence_img in fence_augmentation_list_nohole:
+                            mask = cv2.bitwise_or(rural_scaled_img, rural_scaled_img, mask=fence_img)
+                            result = cv2.add(rural_img, mask)
+                            result = cv2.addWeighted(rural_img, 0.4, mask, 1.0, 0)
+                            cv2.imwrite(f'{self.output_dir}/R{total_rural_index}r{rural_index}F{total_fence_index}f{fence_index}withouthole.png', result)
+                            fence_index += 1
+                            pbar.update(1)
+
+                        rural_index += 1
+                    total_fence_index += 1
+                total_rural_index += 1
                     
-                    i += 1
-                    if (i > num_images):
-                        return
-                    
-                    cv2.imshow("holed_img", fence_holed_img)
-                    cv2.imshow("rural_scaled_img", rural_scaled_img)
-                    cv2.imshow("result", result)
-                    cv2.waitKey(0)
-                    cv2.destroyAllWindows()
             
 image_dir = "raw_images"
-output_dir = "test_outpt"
+output_dir = "test_output"
 datagen = DataGen(image_dir=image_dir, output_dir=output_dir)
-datagen.test(5)
+datagen.image_gen()
 
